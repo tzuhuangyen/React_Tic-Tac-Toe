e0c581ae8c996e7343c04c0b821ec4fed3a091d8
tzuhuangyen <912yan@gmail.com>
2024-07-29 12:06:31 +0200

    css

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Tic-Tac-Toe
 React + Vite
 
 practice
+
+[https://tzuhuangyen.github.io/React_Tic-Tac-Toe/](https://tzuhuangyen.github.io/React_Tic-Tac-Toe/)
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-
+import Board from './components/Board';
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
     <>
       <h1>Tic Tac Toe</h1>
       <h2>Vite + React</h2>
+      <Game />
     </>
   );
 }
